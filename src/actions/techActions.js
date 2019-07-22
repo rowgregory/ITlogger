e323517d55bcd2e8a6a76a9bdc65561e65f745c2
rowgregory <rowgregory@gmail.com>
2019-07-22 10:31:53 -0400

    Add technicians

--- src/actions/techActions.js
+++ src/actions/techActions.js
@@ -25,6 +25,32 @@ export const getTechs = () => async dispatch => {
   }
 };
 
+export const addTech = tech => async dispatch => {
+  try {
+    setLoading();
+
+    //post request
+    const res = await fetch("/techs", {
+      method: "POST",
+      body: JSON.stringify(tech),
+      headers: {
+        "Content-type": "application/json"
+      }
+    });
+    const data = await res.json();
+
+    dispatch({
+      type: ADD_TECH,
+      payload: data
+    });
+  } catch (err) {
+    dispatch({
+      type: TECHS_ERROR,
+      payload: err.response.statusText
+    });
+  }
+};
+
 // Set loading to true
 export const setLoading = () => {
   return {
